f9e1e3e490a76eea93ecde7582e8c881e12ef9ff
Jessica Aquino <63423099+JessicaAquino@users.noreply.github.com>
2020-09-11 21:34:27 -0400

    Update 2_variables.sh

--- ProgramacionBashShell/2_variables/2_variables.sh
+++ ProgramacionBashShell/2_variables/2_variables.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
 #Programa para revisar la declaración de variables
 
-opcion=0
-nombre=Jessica
-
-echo "Opción: $opcion y Nombre: $nombre"
-
-#Exportar la variable nombre para que esté disponible a los demás procesos
-export nombre
-
-#Llamar al siguiente script para recuperar la variable
-./2_variables.sh
+echo "Opción nombre pasada del script anterior: $nombre"
